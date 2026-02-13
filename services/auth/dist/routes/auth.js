86a883a2d8eabe67e6854e1b42113ad69702f406
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-02-13 20:59:56 +0530

    added role routes

--- services/auth/dist/routes/auth.js
+++ services/auth/dist/routes/auth.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { loginUser } from "../controllers/auth.js";
+import { addUserRole, loginUser } from "../controllers/auth.js";
+import { isAuth } from "../middlewares/isAuth.js";
 const router = express.Router();
 router.post("/login", loginUser);
+router.put("/add/role", isAuth, addUserRole);
 export default router;
