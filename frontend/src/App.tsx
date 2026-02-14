3833293d0a95c444b6dd3ea335360ab910076b2c
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-02-14 13:54:20 +0530

    added: Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
