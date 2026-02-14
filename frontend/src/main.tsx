e29974907c4f8b9b45482a4b40f6d37d2c785f65
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-02-15 01:17:39 +0530

    Login Page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,8 @@ export const authService = 'http://localhost:5000';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <GoogleOAuthProvider clientId='406806456636-fgipb36vj7tns8h93fg0r3b5l4dn1p6g.apps.googleusercontent.com'>
     <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -3,7 +3,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom";
 import { authService } from "../main";
 import toast from "react-hot-toast";
-import {GoogleLogin} from "@react-oauth/google"
+import { useGoogleLogin} from "@react-oauth/google"
+import {FcGoogle} from "react-icons/fc"
 
 const Login = () => {
   const [loading,setLoading] = useState(false);
@@ -28,8 +29,30 @@ const Login = () => {
       setLoading(false);
     }
   }
+
+  const googleLogin = useGoogleLogin({
+    onSuccess: responseGoogle,
+    onError: responseGoogle,
+    flow: "auth-code"
+  })
   return (
-    <div>Login</div>
+    <div className="flex min-h-screen items-center justify-center bg-white px-4">
+      <div className="w-full max-w-sm space-y-6">
+        <h1 className="text-center text-3xl font-bold text-[#E23774]">
+          KhaoJi
+        </h1>
+
+        <p className="text-center text-sm text-gray-500">Log in or sign up to continue</p>
+
+        <button onClick={googleLogin} disabled={loading} className="flex w-full items-center justify-center gap-3 rounded-xl border border-gray-300 bg-white px-4"><FcGoogle size={20} />{loading ? "Signing in..." : "Continue with google"}</button>
+
+        <p className="text-center text-xs text-gray-400">
+          By continuing, you agree with our{" "}
+          <span className="text-[#E23774]">Terms of Service</span> & {" "}
+          <span className="text-[#E23774]">Privacy Policy</span>
+        </p>
+      </div>
+    </div>
   )
 }
 
