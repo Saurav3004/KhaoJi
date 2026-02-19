533bb9a8c40952cd608f2df4c38c388e559a0415
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-02-19 10:57:46 +0530

    Added: cloudinary function

--- services/utils/src/index.ts
+++ services/utils/src/index.ts
@@ -1,9 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import cloudinary from "cloudinary"
+import cors from "cors"
+import uploadRoutes from "./routes/cloudinary.js"
 dotenv.config()
 
 const app = express();
+app.use(cors());
+
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: true}));
+
 
 const {CLOUD_NAME, CLOUD_API_KEY, CLOUD_SECRET_KEY} = process.env;
 
@@ -16,6 +23,8 @@ cloudinary.v2.config({
     api_key: CLOUD_API_KEY,
     api_secret: CLOUD_SECRET_KEY
 });
+
+app.use("/api",uploadRoutes)
 
 const PORT = process.env.PORT || 5002;
 
