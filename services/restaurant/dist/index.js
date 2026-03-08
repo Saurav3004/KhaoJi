f41abd2dac75372f0e9d8c1253a5d3395b0f4dab
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2026-03-08 23:15:44 +0530

    Updated the code

--- services/restaurant/dist/index.js
+++ services/restaurant/dist/index.js
@@ -2,8 +2,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
 import connectDB from "./config/db.js";
+import restaurantRoute from "./routes/restaurant.js";
 const app = express();
 const PORT = process.env.PORT || 5001;
+app.use("/api/restaurant", restaurantRoute);
 app.listen(PORT, () => {
     console.log(`Restaurant service is running on port ${PORT}`);
     connectDB();
